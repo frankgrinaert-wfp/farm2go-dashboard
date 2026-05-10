8b225fc17fd0936c5775652ecafc826c1088bca8
Frank Grinaert <frank.grinaert@wfp.org>
2026-05-10 15:49:07 +0200

    app information update

--- src/components/custom/app-information-dialog.tsx
+++ src/components/custom/app-information-dialog.tsx
@@ -23,9 +23,10 @@ function AppInformationDialog({
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="max-w-lg">
         <DialogHeader>
-          <DialogTitle>Farm2Go</DialogTitle>
+          <DialogTitle>App information</DialogTitle>
         </DialogHeader>
-        <div className="flex flex-col gap-4 text-sm">
+        <div className="flex flex-col gap-3 text-sm">
+          <p>Farm2Go</p>
           <p>Version: 2026-04-17T14:56:06.840Z</p>
           <p>2018-2026 &copy; World Food Programme</p>
         </div>
